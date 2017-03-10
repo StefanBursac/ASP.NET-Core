666a572b3e0f00eafb73bfdccb56195cd8ec505f
Stefan <Stefan@MYPC>
2017-03-11 00:25:27 +0100

    prvi komit

---
+++ CityInfo/src/CityInfo.API/CityDataStore.cs
@@ -0,0 +1,34 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CityDataStore
+    {
+        public static CityDataStore Current { get; } = new CityDataStore();
+
+        public List<CityDto> Cities { get; set; }
+        
+        public CityDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The One with that big park"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The One  with catedral that was never finished"
+                }
+            };
+        }
+    }
+}
--- CityInfo/src/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/src/CityInfo.API/Controllers/CitiesController.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
+
     public class CitiesController : Controller
     {
+
+        [HttpGet()]
 
         public JsonResult GetCities()
         {
@@ -16,8 +20,20 @@ namespace CityInfo.API.Controllers
             {
                 new { id = 1 , Name =  "New York City" },
                 new { id = 2, Name = "Antwerp" }
-        }); 
+        });
+        }
+
+        [HttpGet("{id}")]
+
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult
+               (
+                CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+               );
+
 
         }
+
     }
 }
