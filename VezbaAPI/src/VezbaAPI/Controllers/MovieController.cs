c41dfa48c16be9744855c6e32376da65c99e6c43
DOIT_DOTNET_USER8 <DOIT_DOTNET_USER8@C08>
2017-03-11 14:21:48 +0100

    komit iz smarta

---
+++ VezbaAPI/src/VezbaAPI/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VezbaAPI.Models;
+
+namespace VezbaAPI.Controllers
+{
+
+    [Route("")]
+
+    
+
+        public class MovieController : Controller
+    {
+
+        public HelloWorld()
+        {
+            Console.Write("Hello World");
+        }
+
+
+        [HttpGet("{movies}")]
+        public JsonResult GetMovies()
+        {
+            return new JsonResult(new List<object>()
+
+            {
+                new Movie {Name = "m" },
+                new Movie { Name = "mm" }
+            });
+
+            
+            
+        }
+        
+        
+        
+        
+        
+
+
+
+
+    }
+}
